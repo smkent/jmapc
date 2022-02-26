423bfc7148098fdde18fc4314b875e0791f95039
Stephen Kent <smkent@smkent.net>
2022-02-26 10:56:16 -0800

    Remove main function namespacing from examples, enable examples linting

--- examples/echo.py
+++ examples/echo.py
@@ -9,24 +9,18 @@ import os
 from jmapc import Client
 from jmapc.methods import CoreEcho
 
+# Create and configure client
+client = Client(
+    host=os.environ["JMAP_HOST"],
+    user=os.environ["JMAP_USER"],
+    password=os.environ["JMAP_PASSWORD"],
+)
 
-def main() -> None:
-    # Create and configure client
-    j = Client(
-        host=os.environ["JMAP_HOST"],
-        user=os.environ["JMAP_USER"],
-        password=os.environ["JMAP_PASSWORD"],
-    )
+# Prepare a request for the JMAP Core/echo method with some sample data
+method = CoreEcho(data=dict(hello="world"))
 
-    # Prepare a request for the JMAP Core/echo method with some sample data
-    method = CoreEcho(data=dict(hello="world"))
+# Call JMAP API with the prepared request
+result = client.call_method(method)
 
-    # Call JMAP API with the prepared request
-    result = j.call_method(method)
-
-    # Print result
-    print(result)
-
-
-if __name__ == "__main__":
-    main()
+# Print result
+print(result)
--- examples/identity_get.py
+++ examples/identity_get.py
@@ -10,29 +10,23 @@ import os
 from jmapc import Client
 from jmapc.methods import IdentityGet
 
+# Create and configure client
+client = Client(
+    host=os.environ["JMAP_HOST"],
+    user=os.environ["JMAP_USER"],
+    password=os.environ["JMAP_PASSWORD"],
+)
 
-def main() -> None:
-    # Create and configure client
-    j = Client(
-        host=os.environ["JMAP_HOST"],
-        user=os.environ["JMAP_USER"],
-        password=os.environ["JMAP_PASSWORD"],
+# Prepare Identity/get request
+# To retrieve all of the user's identities, no arguments are required.
+method = IdentityGet()
+
+# Call JMAP API with the prepared request
+result = client.call_method(method)
+
+# Print some information about each retrieved identity
+for identity in result.data:
+    print(
+        f"Identity {identity.id} is for "
+        f"{identity.name} at {identity.email}"
     )
-
-    # Prepare Identity/get request
-    # To retrieve all of the user's identities, no arguments are required.
-    method = IdentityGet()
-
-    # Call JMAP API with the prepared request
-    result = j.call_method(method)
-
-    # Print some information about each retrieved identity
-    for identity in result.data:
-        print(
-            f"Identity {identity.id} is for "
-            f"{identity.name} at {identity.email}"
-        )
-
-
-if __name__ == "__main__":
-    main()
