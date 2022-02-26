8a57274ab649af23667462672609b7403677d55a
Stephen Kent <smkent@smkent.net>
2022-02-26 13:42:07 -0800

    Add datetime serialization tests

--- tests/test_serializer.py
+++ tests/test_serializer.py
@@ -1,9 +1,13 @@
 from dataclasses import dataclass, field
-from typing import Optional
+from datetime import datetime, timezone
+from typing import Any, Dict, Optional
+
+import pytest
+from dataclasses_json import config
 
 from jmapc import ResultReference
 from jmapc.models import ListOrRef
-from jmapc.serializer import Model
+from jmapc.serializer import Model, datetime_decode, datetime_encode
 
 
 def test_camel_case() -> None:
@@ -47,3 +51,30 @@ def test_serialize_add_account_id() -> None:
     )
     to_dict = d.to_dict(account_id="u1138")
     assert to_dict == dict(accountId="u1138", data="is beautiful")
+
+
+@pytest.mark.parametrize(
+    ["dt", "expected_dict"],
+    [
+        (
+            datetime(2022, 2, 26, 12, 31, 45, tzinfo=timezone.utc),
+            dict(timestamp="2022-02-26T12:31:45Z"),
+        ),
+        (None, dict()),
+    ],
+)
+def test_serialize_datetime(
+    dt: datetime, expected_dict: Dict[str, Any]
+) -> None:
+    @dataclass
+    class TestModel(Model):
+        timestamp: Optional[datetime] = field(
+            default=None,
+            metadata=config(encoder=datetime_encode, decoder=datetime_decode),
+        )
+
+    d = TestModel(timestamp=dt)
+    to_dict = d.to_dict()
+    assert to_dict == expected_dict
+    from_dict = TestModel.from_dict(to_dict)
+    assert from_dict == d
