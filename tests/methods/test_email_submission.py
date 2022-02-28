039a33876e03d6b473f00c932724e30c5f7ada64
Stephen Kent <smkent@smkent.net>
2022-02-27 21:31:38 -0800

    Add default value for SetError.description

--- tests/methods/test_email_submission.py
+++ tests/methods/test_email_submission.py
@@ -2,7 +2,14 @@ from datetime import datetime, timezone
 
 import responses
 
-from jmapc import Address, Client, EmailSubmission, Envelope, UndoStatus
+from jmapc import (
+    Address,
+    Client,
+    EmailSubmission,
+    Envelope,
+    SetError,
+    UndoStatus,
+)
 from jmapc.methods import (
     EmailSetResponse,
     EmailSubmissionSet,
@@ -296,6 +303,114 @@ def test_email_submission_set_on_success_update_email(
             destroyed=None,
             not_created=None,
             not_updated=None,
+            not_destroyed=None,
+        ),
+    ]
+
+
+def test_email_submission_set_update_email_error(
+    client: Client, http_responses: responses.RequestsMock
+) -> None:
+    expected_request = {
+        "methodCalls": [
+            [
+                "EmailSubmission/set",
+                {
+                    "accountId": "u1138",
+                    "create": expected_request_create,
+                    "onSuccessUpdateEmail": {
+                        "keywords/$draft": None,
+                        "mailboxIds/MBX5": None,
+                    },
+                },
+                "uno",
+            ]
+        ],
+        "using": [
+            "urn:ietf:params:jmap:core",
+            "urn:ietf:params:jmap:submission",
+        ],
+    }
+    response = {
+        "methodResponses": [
+            [
+                "EmailSubmission/set",
+                email_submission_set_response,
+                "uno",
+            ],
+            [
+                "Email/set",
+                {
+                    "accountId": "u1138",
+                    "oldState": "2",
+                    "newState": "3",
+                    "created": None,
+                    "updated": None,
+                    "destroyed": None,
+                    "notCreated": None,
+                    "notUpdated": {
+                        "Mdeadbeefdeadbeefdeadbeef": {
+                            "type": "invalidProperties",
+                            "properties": ["mailboxIds"],
+                        },
+                    },
+                    "notDestroyed": None,
+                },
+                "uno",
+            ],
+        ]
+    }
+    expect_jmap_call(http_responses, expected_request, response)
+    assert client.method_call(
+        EmailSubmissionSet(
+            on_success_update_email={
+                "keywords/$draft": None,
+                "mailboxIds/MBX5": None,
+            },
+            create=dict(
+                emailToSend=EmailSubmission(
+                    email_id="#draft",
+                    identity_id="1000",
+                    envelope=Envelope(
+                        mail_from=Address(email="ness@onett.example.com"),
+                        rcpt_to=[Address(email="ness@onett.example.com")],
+                    ),
+                )
+            ),
+        )
+    ) == [
+        EmailSubmissionSetResponse(
+            account_id="u1138",
+            old_state="1",
+            new_state="2",
+            created=dict(
+                emailToSend=EmailSubmission(
+                    id="S2000",
+                    undo_status=UndoStatus.FINAL,
+                    send_at=datetime(
+                        1994, 8, 24, 12, 1, 2, tzinfo=timezone.utc
+                    ),
+                ),
+            ),
+            updated=None,
+            destroyed=None,
+            not_created=None,
+            not_updated=None,
+            not_destroyed=None,
+        ),
+        EmailSetResponse(
+            account_id="u1138",
+            old_state="2",
+            new_state="3",
+            created=None,
+            updated=None,
+            destroyed=None,
+            not_created=None,
+            not_updated={
+                "Mdeadbeefdeadbeefdeadbeef": SetError(
+                    type="invalidProperties", description=None
+                )
+            },
             not_destroyed=None,
         ),
     ]
