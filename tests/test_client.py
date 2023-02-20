d8c5f51c415a380cd9f77eecfcd85bf32289f784
Stephen Kent <smkent@smkent.net>
2023-02-19 21:30:56 -0800

    Add test coverage for missing download destination file name

--- tests/test_client.py
+++ tests/test_client.py
@@ -398,6 +398,15 @@ def test_download_attachment(
         body=blob_content,
     )
     dest_file = tempdir / "download.txt"
+    with pytest.raises(Exception) as e:
+        client.download_attachment(
+            EmailBodyPart(
+                name="download.txt", blob_id="C2187", type="text/plain"
+            ),
+            "",
+        )
+    assert str(e.value) == "Destination file name is required"
+    assert not dest_file.exists()
     client.download_attachment(
         EmailBodyPart(name="download.txt", blob_id="C2187", type="text/plain"),
         dest_file,
